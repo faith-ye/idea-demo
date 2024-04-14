f9c41dd4cbf837af8e66fcf897d94d712981d97f
faith-ye <m202171620@hust.edu.cn>
2024-04-14 14:52:33 +0800

    refactor calculator code using reflect

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -5,7 +5,7 @@
  * @date: 2024/4/14 13:55
  */
 public class Calculator {
-    public int compute(int num1, int num2, char symbol) {
+    public int compute(int num1, int num2, String symbol) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
         Computable computable = ComputeFactory.getCompute(symbol);
         return computable.compute(num1, num2);
     }
--- src/main/java/ComputeFactory.java
+++ src/main/java/ComputeFactory.java
@@ -8,14 +8,8 @@ public class ComputeFactory {
     private static final Sub sub = new Sub();
     private static final Add add = new Add();
 
-    public static Computable getCompute(char symbol){
-        switch (symbol){
-            case '+':
-                return add;
-            case '-':
-                return sub;
-            default:
-                throw new IllegalArgumentException();
-        }
+    public static Computable getCompute(String symbol)
+            throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+        return (Computable) Class.forName(symbol).newInstance();
     }
 }
