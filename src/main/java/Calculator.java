3a1a37466fd2a042a60dff1c1caef42e8ecf6ec6
faith-ye <m202171620@hust.edu.cn>
2024-04-14 14:07:22 +0800

    add calculator test

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,11 @@
+/**
+ * @className: Calculator
+ * @description: TODO
+ * @author: faith_ye
+ * @date: 2024/4/14 13:55
+ */
+public class Calculator {
+    public int compute(int num1, int num2, char symbol) {
+        return -1;
+    }
+}
